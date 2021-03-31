3f1fb1f1654e8c05d94cc8ed1270a7f066d6c8c4
adeelmalik78 <amalik@datical.com>
2021-03-31 15:59:25 -0500

    removed update.sh

--- update.sh
+++
@@ -1,3 +0,0 @@
-LB_ARGS="--classpath=/workspace/example/changelogs:/workspace/example/changelogs/ojdbc8.jar --changeLogFile=changelog.oracle.sql --defaultsFile=/workspace/liquibase.properties"
-echo "liquibase/liquibase $LB_ARGS status --verbose"
-liquibase/liquibase $LB_ARGS status --verbose

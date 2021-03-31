031f124d4eb684bfebf34fe58b4f8c74da9e71e2
adeelmalik78 <amalik@datical.com>
2021-03-31 12:49:37 -0500

    update script

---
+++ update.sh
@@ -0,0 +1,3 @@
+LB_ARGS="--classpath=/workspace/example/changelogs:/workspace/example/changelogs/ojdbc8.jar --changeLogFile=changelog.oracle.sql --defaultsFile=/workspace/liquibase.properties"
+echo "liquibase/liquibase $LB_ARGS status --verbose"
+liquibase/liquibase $LB_ARGS status --verbose"
